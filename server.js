271c0482df516ef826850f7a41f55aeefec735c5
helaman-Y-B <90625373+helaman-Y-B@users.noreply.github.com>
2025-04-24 01:18:34 +0000

    Started to render the main layout page

---
+++ server.js
@@ -0,0 +1,33 @@
+// server.js is responsable to control the project.
+
+/* ***********************
+ * Require Statements
+ *************************/
+const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
+const env = require("dotenv").config();
+
+// Creates the app instance
+const app = express();
+
+/* ***********************
+ * View engine and template
+ *************************/
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "main");
+
+// Render the main layout
+app.get("/", (req, res) => {
+    res.render("main");
+});
+
+// .env items
+const port = process.env.PORT; 
+const host = process.env.HOST;
+
+// Listens to the port and host
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port} and host ${host}`);
+});
+
