5c4011073a66f055253141cc19b27c9cccf4679a
helaman-Y-B <90625373+helaman-Y-B@users.noreply.github.com>
2025-04-26 00:33:43 +0000

    made half of the animation for the arrow nav

--- utils/utils.js
+++ utils/utils.js
@@ -5,6 +5,7 @@ const Utils = {};
  ************************** */
 Utils.getNavBar = async function() {
     let nav = "<ul id='navBar'>"
+    nav += "<li><img id='nav-arrow' src='/img/nav-arrow.png' alt='arrow'/></li>"
     nav += "<li><a href='/'>Home</a></li>"
     nav += "<li><a href='/'>Sobre</a></li>"
     nav += "<li><a href='/'>Contato</a></li>"
