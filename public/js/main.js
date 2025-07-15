cf4e8b145a2edf994df977cd6616883b31cc2e5b
helaman-Y-B <90625373+helaman-Y-B@users.noreply.github.com>
2025-07-15 01:32:54 +0000

    Finished fixing the navbar since it was getting stuck when clicking it in small screens.
    Added an event listener for the nav element so that when the screen width is smaller then 768 pixels, then it uses an "click" eventListener and when the screen width surpasses the 768, then the screen uses "mauseover" eventListener.
    
    Also made so that every time the screen resizes, an eventListerner will trigger and refresh the page so that the page uses hover and not click.

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,37 @@
-document.getElementById("nav").addEventListener("mouseover", () => {
-    const navItems = document.getElementsByClassName("nav");
-    for (let item of navItems) {
-        item.style.display = "block"; // Show all nav items
+let screenWidth = window.innerWidth
+
+window.addEventListener("resize", () => {
+    if (screenWidth !== window.innerWidth) {
+        window.location.reload(); // Reload the page on resize to apply new styles
     }
-});
+})
 
 
-document.getElementById("nav").addEventListener("mouseout", () => {
-    const navItems = document.getElementsByClassName("nav");
-    for (let item of navItems) {
-        item.style.display = "none"; // Hide all nav items
-    }
-});
+if (screenWidth < 768 ) {
+    document.getElementById("nav").addEventListener("click", () => {
+        const navItems = document.getElementsByClassName("nav");
+        for (let item of navItems) {
+            if (item.style.display === "block") {
+                item.style.display = "none"; // Hide nav items
+                document.getElementById("nav-arrow").style.transform = "rotate(0deg)"; // Reset arrow rotation
+            } else {
+                item.style.display = "block"; // Show nav items
+                document.getElementById("nav-arrow").style.transform = "rotate(180deg)"; // Rotate arrow
+            }
+        }
+    })
+} else {
+    document.getElementById("nav").addEventListener("mouseover", () => {
+        const navItems = document.getElementsByClassName("nav");
+        for (let item of navItems) {
+            item.style.display = "block"; // Show all nav items
+        }
+    });
+
+    document.getElementById("nav").addEventListener("mouseout", () => {
+        const navItems = document.getElementsByClassName("nav");
+        for (let item of navItems) {
+            item.style.display = "none"; // Hide all nav items
+        }
+    });
+}
