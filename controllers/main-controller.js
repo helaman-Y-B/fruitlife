68abec9913ae39fd6a19e129270446239cb58b14
Helamã Y. Barbour <helamashi@gmail.com>
2025-07-24 11:15:44 -0300

    Trying to fix the nav bar sin in bigger screens, it's having a hover effect.

--- controllers/main-controller.js
+++ controllers/main-controller.js
@@ -10,7 +10,7 @@ mainController.getMainPage = async function(req, res) {
     const navBar = await utilities.getNavBar();
     const fruits_for_sale = await getFruits();
 
-    console.log("Fruits for sale:", fruits_for_sale.rows); // Log the rows to check the data
+    //console.log("Fruits for sale:", fruits_for_sale.rows); // Log the rows to check the data
 
     res.render("main", {
         title: "Main Page",
